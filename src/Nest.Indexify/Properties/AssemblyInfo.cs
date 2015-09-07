bf84a129d107092624e12e93bbd85cb70e7a3df7
Phil Oyston <oystonpj@oystec.co.uk>
2015-09-07 13:12:31 +0100

    Updates build to use GitVersionTask

--- src/Nest.Indexify/Properties/AssemblyInfo.cs
+++ src/Nest.Indexify/Properties/AssemblyInfo.cs
@@ -12,8 +12,4 @@ using System.Runtime.InteropServices;
 #endif
 
 [assembly: AssemblyProduct("Nest.Queryify")]
-
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-
 [assembly: ComVisible(false)]
